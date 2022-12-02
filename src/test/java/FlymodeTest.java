e729d30cd24fd569b1deb16cba1cf9d773b08e0f
Eduard Barzilovic <edjka92@gmail.com>
2022-12-02 21:32:26 +0300

    Amazon test

--- src/test/java/FlymodeTest.java
+++ src/test/java/FlymodeTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.v85.input.Input;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
